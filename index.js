3bc2c5261ae9de6e9e01833dfe1961068ead628e
Ryoya Kawai <ryoya.kawai@gmail.com>
2022-04-19 19:57:22 +0900

    update: update directory stricture

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const _BASE_DIR = __dirname
-const _HTML_DIR = `${_BASE_DIR}/web_server/html_content/`
-
-const server = require('./web_server/index.js');
-
-
-console.log()
-server.startWebServer(_HTML_DIR)
