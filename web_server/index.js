8b18b9f7d8df75b68f1e3bc6c17a7a6afaff4bf4
Ryoya Kawai <ryoya.kawai@gmail.com>
2022-04-19 20:45:44 +0900

    Initial import

--- web_server/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const _BASE_DIR = __dirname
-const _HTML_DIR = `${_BASE_DIR}/../dist/`
-
-const server = require('./server.js');
-
-server.startWebServer(_HTML_DIR)
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entry: `${__dirname}/src/scripts/main.js`,
-  output: {
-    path: `${__dirname}/dist/scripts/`,
-    filename: 'bundle.js'
-  },
-  cache: true
-};
-
