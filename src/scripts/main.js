d00f2146fc8c8ee965e5ccbaa15731a5f95e3097
Ryoya Kawai <ryoya.kawai@gmail.com>
2022-04-21 16:23:30 +0900

    update: update timing to stop slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,19 +30,18 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
     toggle_button_disable(seek_bar)
   }
 
-  const toggle_display_time = (callback = () => { }) => {
-    if (isPlaying) {
-      seekTimerId = setInterval(() => {
-        if (wa.source.buffer !== null) {
-          const duration = wa.source.buffer.duration.toFixed(2)
-          const current_position = (wa.a_ctx_paused_time + wa.a_ctx.currentTime - wa.a_ctx_start_time).toFixed(4)
-          callback(current_position, duration)
-        }
-      }, SEEK_DURATION_MSEC)
-    } else {
-      clearInterval(seekTimerId)
-    }
+  const start_update_display_time = (callback = () => { }) => {
+    seekTimerId = setInterval(() => {
+      if (wa.source.buffer !== null) {
+        const duration = wa.source.buffer.duration.toFixed(2)
+        const current_position = (wa.a_ctx_paused_time + wa.a_ctx.currentTime - wa.a_ctx_start_time).toFixed(4)
+        callback(current_position, duration)
+      }
+    }, SEEK_DURATION_MSEC)
   }
+  const stop_update_display_time = () => (
+    clearInterval(seekTimerId)
+  )
 
   const setPausedTime = () => {
     wa.a_ctx_start_time = wa.a_ctx.currentTime - wa.a_ctx_start_time
@@ -67,11 +66,13 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 
     //
     seek_bar.addEventListener('input', async (event) => {
-      const duration = wa.source.buffer.duration
-      const seek_position = duration * event.target.value / 100
-      wa.a_ctx_paused_time = seek_position
-      const [hour, min, sec, msec] = convTimeFormatFromSec(seek_position)
-      seek_bar_text.innerHTML = `${hour}:${min}:${sec}:${msec}`
+      if (typeof wa.source.buffer.duration !== null) {
+        const duration = wa.source.buffer.duration
+        const seek_position = duration * event.target.value / 100
+        wa.a_ctx_paused_time = seek_position
+        const [hour, min, sec, msec] = convTimeFormatFromSec(seek_position)
+        seek_bar_text.innerHTML = `${hour}:${min}:${sec}:${msec}`
+      }
     })
 
     //
@@ -90,20 +91,20 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
       if (!isPlaying) {
         toggle_button_disable(seek_bar)
         toggle_button.innerHTML = '■ Stop'
-        //wa.source = await decodeAudioDataPromise(wa)
         wa.source.connect(wa.a_ctx.destination)
         wa.source.start(0, wa.a_ctx_paused_time + wa.a_ctx_start_time)
         setPausedTime()
+        start_update_display_time(seek_bar_callback)
       } else {
         toggle_button.innerHTML = '▶ Start'
         setPausedTime()
-        toggle_button_disable(seek_bar)
+        stop_update_display_time(seek_bar_callback)
         wa.source.stop(0)
+        wa.source.buffer = null
         wa.source = await decodeAudioDataPromise(wa)
-        //wa.source.buffer = null
+        toggle_button_disable(seek_bar)
       }
       isPlaying = !isPlaying
-      toggle_display_time(seek_bar_callback)
     })
   }
 
