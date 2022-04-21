cb2cc830e7fd956ebe87b9274e2b802923084193
Ryoya Kawai <ryoya.kawai@gmail.com>
2022-04-21 22:47:06 +0900

    add: add time stretch with kali https://github.com/danigb/timestretch

--- src/scripts/loadaudio.js
+++ src/scripts/loadaudio.js
@@ -1,4 +1,5 @@
 "use strict";
+import { doStretch } from './kali_wrapper.js';
 
 const fetchAudio = async (file_location = null) => {
   if (file_location == null) {
@@ -27,7 +28,34 @@ const decodeAudioDataPromise = async (wa = {}) => {
   })
 }
 
+const decodeAudioDataStretchPromise = async (wa = {}, rate = 1, quickseek = true) => {
+  const source = wa.a_ctx.createBufferSource()
+  const arrayBuffer = wa.arrayBuffer.slice()
+  return new Promise((resolve) => {
+    wa.a_ctx.decodeAudioData(arrayBuffer, (decodedBuffer) => {
+      // do stretch
+      console.log('[info] begin stretch and load audio')
+      const inputData_0 = decodedBuffer.getChannelData(0);
+      const inputData_1 = decodedBuffer.getChannelData(1);
+      const output_0 = doStretch(wa.a_ctx, inputData_0, parseFloat(rate), 1, quickseek);
+      const output_1 = doStretch(wa.a_ctx, inputData_1, parseFloat(rate), 1, quickseek);
+
+      const outputAudioBuffer = wa.a_ctx.createBuffer(2, output_0.length, wa.a_ctx.sampleRate)
+      outputAudioBuffer.getChannelData(0).set(output_0)
+      outputAudioBuffer.getChannelData(1).set(output_1)
+
+      source.buffer = outputAudioBuffer
+      source.loop = false
+
+      console.log('[info] completed to strech and load audio')
+      resolve(source)
+    })
+  })
+}
+
+
 export {
   fetchAudio,
-  decodeAudioDataPromise
+  decodeAudioDataPromise,
+  decodeAudioDataStretchPromise
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,14 @@
 "use strict";
 
-import { fetchAudio, decodeAudioDataPromise } from './loadaudio.js';
-import { convTimeFormatFromSec, toggle_button_disable } from './mainlib.js';
+import {fetchAudio, decodeAudioDataPromise, decodeAudioDataStretchPromise} from './loadaudio.js';
+import {convTimeFormatFromSec, toggle_button_disable} from './mainlib.js';
 
 const _MP3_URL = './mp3/eine.mp3';
 const SEEK_DURATION_MSEC = 30
 const wa = {
   a_ctx: null,
   source: null,
+  file_name: '',
   arrayBuffer: null,
   a_ctx_start_time: 0,
   a_ctx_paused_time: 0
@@ -19,21 +20,26 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 
 (async () => {
   // Get UI elements
-  const seek_bar = document.querySelector('#seek_bar')
+  const seek_bar = document.querySelector('input#seek_bar')
   const seek_bar_text = document.querySelector('span#seek_bar_text')
-  const loadaudio_button = document.querySelector('#loadaudio_button')
-  const start_stop_button = document.querySelector('#start_stop_button')
+  const loadaudio_button = document.querySelector('button#loadaudio_button')
+  const start_stop_button = document.querySelector('button#start_stop_button')
+  const playback_rate = document.querySelector('input#playback_rate')
+  const playback_rate_text = document.querySelector('span#playback_rate_text')
 
   const allow_play = async () => {
     wa.a_ctx = new AudioContext()
     try {
       wa.arrayBuffer = await fetchAudio(_MP3_URL)
+      wa.file_name = _MP3_URL.split('/').pop()
+      start_stop_button.innerHTML = `▶ Start : ${wa.file_name}`
+      console.log(`[message] ${_MP3_URL} is loaded.`)
+      wa.source = await decodeAudioDataPromise(wa)
+      toggle_button_disable(seek_bar)
+      loadaudio_button.innerHTML = 'Reload Page To Change Music'
     } catch (err) {
       console.error(`[ERROR] allow_play: msg=[${JSON.stringify(err)}]`)
     }
-    console.log(`[message] ${_MP3_URL} is loaded.`)
-    wa.source = await decodeAudioDataPromise(wa)
-    toggle_button_disable(seek_bar)
   }
 
   const start_update_display_time = (callback = () => { }) => {
@@ -59,10 +65,44 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
     seek_bar_text.innerHTML = `${hour}:${min}:${sec}:${msec}`
   }
 
+  const uploadFilDragOver = (event) => {
+    event.stopPropagation()
+    event.preventDefault()
+
+    event.dataTransfer.dropEffect = 'copy';
+    event.target.setAttribute('disabled', 'disabled')
+  }
+  const uploadFilDragLeave = (event) => {
+    event.stopPropagation()
+    event.preventDefault()
+
+    event.target.removeAttribute('disabled')
+  }
+  const uploadFileDrop = (event) => {
+    event.stopPropagation()
+    event.preventDefault()
+
+    const file = event.dataTransfer.files[0]
+    wa.file_name = file.name
+    const reader = new FileReader()
+    reader.onload = async (ev) => {
+      wa.arrayBuffer = ev.target.result
+      wa.a_ctx = new AudioContext()
+      console.log(`[message] ${file.name} is loaded.`)
+      wa.source = await decodeAudioDataPromise(wa)
+      start_stop_button.innerHTML = `▶ Start : ${wa.file_name}`
+      toggle_button_disable(start_stop_button)
+      toggle_button_disable(playback_rate)
+      loadaudio_button.innerHTML = 'Reload Page To Change Music'
+    }
+    reader.readAsArrayBuffer(file)
+  }
+
   const main = () => {
     // Init UI
     toggle_button_disable(start_stop_button)
     toggle_button_disable(seek_bar)
+    toggle_button_disable(playback_rate)
 
     //
     seek_bar.addEventListener('input', async (event) => {
@@ -76,36 +116,58 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
     })
 
     //
+    playback_rate.addEventListener('input', async (event) => {
+      playback_rate_text.innerHTML = (Number(event.target.value)).toFixed(2)
+    })
+    playback_rate.addEventListener('change', async (event) => {
+      start_stop_button.innerHTML = `Processing ${wa.file_name}...`
+      toggle_button_disable(start_stop_button)
+      toggle_button_disable(seek_bar)
+      toggle_button_disable(playback_rate)
+      wa.source = await decodeAudioDataStretchPromise(wa, event.target.value, false)
+      start_stop_button.innerHTML = `▶ Start : ${wa.file_name}`
+      toggle_button_disable(start_stop_button)
+      toggle_button_disable(seek_bar)
+      toggle_button_disable(playback_rate)
+    })
+
+    //
+    loadaudio_button.addEventListener('dragover', uploadFilDragOver, false);
+    loadaudio_button.addEventListener('dragleave', uploadFilDragLeave, false);
+    loadaudio_button.addEventListener('drop', uploadFileDrop, false);
     loadaudio_button.addEventListener('mousedown', async () => {
       toggle_button_disable(loadaudio_button)
       try {
         await allow_play()
+        toggle_button_disable(start_stop_button)
+        toggle_button_disable(playback_rate)
       } catch (err) {
         console.error(`[ERROR] loadaudio_button: msg=[${JSON.stringify(err)}]`)
       }
-      toggle_button_disable(start_stop_button)
     })
 
     //
     start_stop_button.addEventListener('mousedown', async () => {
       if (!isPlaying) {
         toggle_button_disable(seek_bar)
-        start_stop_button.innerHTML = '■ Stop'
+        toggle_button_disable(playback_rate)
+        start_stop_button.innerHTML = `■ Stop : ${wa.file_name}`
         wa.source.connect(wa.a_ctx.destination)
         wa.source.start(0, wa.a_ctx_paused_time + wa.a_ctx_start_time)
         setPausedTime()
         start_update_display_time(seek_bar_callback)
       } else {
         toggle_button_disable(start_stop_button)
-        start_stop_button.innerHTML = '- - - -'
+        start_stop_button.innerHTML = `Processing : ${wa.file_name}...`
         setPausedTime()
         stop_update_display_time(seek_bar_callback)
         wa.source.stop(0)
         wa.source.buffer = null
         wa.source = await decodeAudioDataPromise(wa)
+        start_stop_button.innerHTML = `▶ Start : ${wa.file_name}`
         toggle_button_disable(start_stop_button)
-        start_stop_button.innerHTML = '▶ Start'
         toggle_button_disable(seek_bar)
+        toggle_button_disable(playback_rate)
       }
       isPlaying = !isPlaying
     })
